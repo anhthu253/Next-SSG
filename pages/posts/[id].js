840fc1c390cd3ae52e58d5f7b3ed5217446578f8
Thu Nguyen <thu_nguyen@gmx.de>
2022-11-17 12:11:24 +0100

    challenges-next-js-ssg

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,22 +1,51 @@
 import Head from "next/head";
 
+import Link from "next/link";
+
+import { getAllPosts, getPostById } from "../../services/postService";
+
 /*
  * Make all necessary imports.
  * Write the function getStaticPaths.
  * Write the function getStaticProps.
  * Pass down your props to the component.
  * Render the data.
+ *
  */
+/* export async function getStaticPaths() {
+  const posts = await getAllPosts();
+  const ids = posts.map((post) => post.id);
+  return {
+    paths: ids.map((id) => ({ params: { id: id } })),
+    fallback: false,
+  };
+} */
 
-export default function Post() {
+/* export async function getStaticProps(context) {
+  const { id } = context.params;
+  const post = await getPostById(id);
+  return {
+    props: { description: post.description, id: post.id, name: post.name },
+  };
+} */
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const post = await getPostById(id);
+  return {
+    props: { ...post },
+  };
+}
+export default function Post({ name, id, description }) {
   return (
     <>
       <Head>
         <title>title</title>
       </Head>
-      <h1>name of the post</h1>
-      <p>ID:</p>
-      <h2>Description</h2>
+      <h1>{name}</h1>
+      <p>{id}</p>
+      <h2>{description}</h2>
+
       <p></p>
     </>
   );
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,13 +1,30 @@
 import Head from "next/head";
 
+import Link from "next/link";
+
+import { getAllPosts } from "../../services/postService";
 /*
  * Make all necessary imports.
  * Write the function getStaticProps.
  * Pass down your props to the component.
  * Render the data.
  */
+/* export async function getStaticProps() {
+  const posts = await getAllPosts();
+  return {
+    props: { posts: posts },
+  };
+} */
 
-export default function Posts() {
+export async function getServerSideProps() {
+  const posts = await getAllPosts();
+
+  return {
+    props: { posts: posts },
+  };
+}
+
+export default function Posts({ posts }) {
   return (
     <>
       <Head>
@@ -15,7 +32,14 @@ export default function Posts() {
       </Head>
       <h1>All Posts</h1>
       <p>List of all posts</p>
-      <ul></ul>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>{post.name}</Link>
+            <h2>{post.description}</h2>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
